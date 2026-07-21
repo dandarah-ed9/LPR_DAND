42699c400dd3542800889adc591f8e4a1a9837c8
dandarah-ed9 <dandarahmagalhaes9@gmail.com>
2026-07-21 15:32:15 -0300

    AULA2

--- AULA2/EX4_4/Program.cs
+++ AULA2/EX4_4/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Ler o código de uma peça 1, o número de peças 1 , o valor unitário de cada peça 1,
+//o código de uma peça 2 , o numero de peças 2.
+//Calcule e mostre o valor a ser pago.
+
+using System;
+
+namespace EX3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+        }
+
+    }
+}
