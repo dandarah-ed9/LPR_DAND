0d5ffa52bd15664e27e50a59f7ff59ddfc096b51
dandarah-ed9 <dandarahmagalhaes9@gmail.com>
2026-07-21 12:15:01 -0300

    AULA2

--- AULA2/EX1_4.cpp
+++ AULA2/EX1_4.cpp
@@ -1,7 +1,15 @@
+// Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo
+//Fórmula da área: area = π . raio2
+//Considere o valor de π = 3.14159
+
 #include <iostream>
 
 int main() 
 {
+    int raio;
+    double Area = 3.1415* raio *raio;  
     
+   std:: cout << "Digite o valor do raio: " << raio << " O valor da Area é: " << Area << std::endl;
+   std:: cin >> raio;
     return 0;
 }
