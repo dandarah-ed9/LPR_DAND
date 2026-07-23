f473a2ddc686919a557d9a1a42ec297b03dda5f2
dandarah-ed9 <dandarahmagalhaes9@gmail.com>
2026-07-23 20:22:44 -0300

    AULA5

--- AULA5/EX1_5.cpp
+++ AULA5/EX1_5.cpp
@@ -5,25 +5,35 @@
 int main()
 {
    int quantidade;
-   int i = 0;
-   int n1, ni;
-   int media;
-   int numerosTotais;
+   int i = 0; // sempre inicia no 0
+   int numero;
+   int numerosPares;
+   double media;
+   double coletor = 0;
 
    cout << "Digite a quantidade de numeros que sera fornecido: " << endl;
    cin>> quantidade;
 
-   cout << "Digite os numeros : " << endl;
-   cin>> numerosTotais;
+   
+   // sem aspas, se tiver ele le como se fosse 
+   //uma string não uma condição
+   while( i < quantidade )
+   {
+      cout << "Digite os numeros : " << endl;
+      cin >> numero;      // le os numeros
 
-   while(" i <= quantidades ")
-    {
-      if(i %2 ==0 )
+      if(numero %2 ==0 )
       {
-       media =  (n1 + ni)/quantidade ;
+        coletor += numero; // += serve para somar
+        numerosPares++ ;   //pega somente os numeros pares
       }
 
-        i++ ;
-    }   
+        i++ ;   // contador
+    } 
+    
+    media = coletor/numerosPares;
+    cout<< "A media dos numeros digitados e: "<< media << endl;
+
+    return 0 ;
 }   
     
