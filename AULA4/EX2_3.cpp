2a2269657fd5f8be1970be4602574b99a199012d
dandarah-ed9 <dandarahmagalhaes9@gmail.com>
2026-07-22 22:43:59 -0300

    AULA4

--- AULA4/EX2_3.cpp
+++ AULA4/EX2_3.cpp
@@ -3,3 +3,32 @@
 //se os valores lidos são múltiplos  entre si. Atenção: 
 // os números podem ser digitados em ordem crescente ou 
 //decrescente.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A , B;
+
+   cout<< "Digite um numero para  A: "  << endl;
+   cin>> A;
+   
+   cout<< "Digite um numero para  B: "  << endl;
+   cin>> B;
+
+//deixar sem aspas dentro dos parenteses.
+//pois se tiver o programa entende que tudo 
+//que estiver dentro do parenteses é uma string e não uma condição.
+
+   if ( A %  B == 0  ||  B %  A == 0 )
+   {
+    cout<< " Os numeros sao multiplos "  << endl;
+   }
+
+   else 
+   {
+    cout<< " Os numeros nao sao multiplos "  << endl;
+   }
+}
