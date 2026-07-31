1d5afd8bfa22a12f96255c75f9e5cefa1b73d74b
dandarah-ed9 <dandarahmagalhaes9@gmail.com>
2026-07-31 00:15:09 -0300

    AULA 5

--- AULA5/EX3_5.cpp
+++ AULA5/EX3_5.cpp
@@ -1,11 +1,24 @@
+//Elabore um algoritmo que calcule a soma de todos 
+//os números ímpares múltiplos de 3 que se encontrem 
+//no conjunto dos números de 50 a 500.
+
 #include <iostream>
 
    using namespace std ;
     
 int main()
 {
+    int numero;
+    int i= 0;
+    int contador = 0;
 
-
-    
-    return 0;
+    // em vez de numero aqui é i ,por isso que tava dando 0.
+    for(int i = 50 ; i <= 500; i++)
+    {
+        if( i %2 != 0 && i %3 == 0)
+        {
+            contador += i;
+        }
+    }
+    cout <<"A soma de todos os numeros impares sao: "<< contador << endl;
 }
