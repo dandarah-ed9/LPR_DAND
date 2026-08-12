63e518af0d37813c6fdbb804809f674b10d69e71
dandarah-ed9 <dandarahmagalhaes9@gmail.com>
2026-08-12 13:08:25 -0300

    AULAS

---
+++ AULA6/EX3_3/Program.cs
@@ -0,0 +1,29 @@
+﻿//Desenvolva um programa em C# que simule um sistema 
+//de seleção de heróis da Marvel para uma equipe. 
+//O programa deve ter as seguintes funcionalidades:
+//a) Cadastro de Heróis:   
+//b) Seleção de Equipe:    
+//c) Pontuação Total da Equipe:     
+//d) Exibição da Equipe:          
+//e) Menu:           
+//COM Requisitos Adicionais:
+
+using System;
+
+namespace  EX3_3
+{
+    class Program
+    {
+        static void Main(String [] args)
+        {
+
+
+
+
+
+
+          
+            
+        }
+    }
+}
