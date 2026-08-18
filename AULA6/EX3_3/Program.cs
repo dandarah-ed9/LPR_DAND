cca9efc7af92ba7f08cb69f599cd35c5522a87a4
dandarah-ed9 <dandarahmagalhaes9@gmail.com>
2026-08-17 23:25:30 -0300

    AULAS

--- AULA6/EX3_3/Program.cs
+++ AULA6/EX3_3/Program.cs
@@ -14,6 +14,41 @@ namespace  EX3_3
 {
     class Program
     {
+        void superHerois()
+        {
+          string  superHerois1 , superHerois2 ,superHerois3 ,superHerois4 ,superHerois5;
+          string  poder1 , poder2 ,poder3 ,poder4 ,poder5;
+          int  pontuacao1 , pontuacao2 ,pontuacao3 ,pontuacao4 , pontuacao5;
+
+            for(int i = 1; i<=5; i++)
+            {
+            Console.WriteLine("digite o nome do superheroi A: ");
+            superHerois1= Console.ReadLine();
+
+            Console.WriteLine("digite o poder do superheroi A: ");
+            poder1= Console.ReadLine();
+
+            Console.WriteLine("digite a pontuacao  do superheroi A: ");
+            pontuacao1= Console.ReadLine();
+            }
+        }
+        void sel_equipe()
+        {
+            
+        }
+        void pont_total()
+        {
+            
+        }
+        void exib_equipe()
+        {
+            
+        }
+        void menuPrincipal()
+        {
+            
+        }
+
         static void Main(String [] args)
         {
 
