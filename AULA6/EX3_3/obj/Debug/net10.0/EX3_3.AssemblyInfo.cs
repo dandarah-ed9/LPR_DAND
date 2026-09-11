71e825180d53105871200408fb61dadabf6fbf7c
dandarah-ed9 <dandarahmagalhaes9@gmail.com>
2026-09-11 13:41:57 -0300

    AULAS

--- AULA6/EX3_3/obj/Debug/net10.0/EX3_3.AssemblyInfo.cs
+++ AULA6/EX3_3/obj/Debug/net10.0/EX3_3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EX3_3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c1f96bf0c39b7166e1a4a6e807982cb737fa7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+825cb7790021ebdd41002b0f00aa1307d2de1122")]
 [assembly: System.Reflection.AssemblyProductAttribute("EX3_3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EX3_3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
